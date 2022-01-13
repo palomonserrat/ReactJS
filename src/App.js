36293513003e0d4e86a463e7a1ee04b9a39d714b
Paloma Porco <paloporco@gmail.com>
2022-01-13 09:57:23 -0300

    contador

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, {useState} from 'react';
 
 function App() {
+  const [itemsQty, setItemsQty] = useState(0);
   return (
     <div className="App">
       <NavBar />
@@ -13,6 +17,9 @@ function App() {
         <p>
           Mi Primer Tienda!
         </p>
+        
+        <div className="app">
+    </div>
         <a
           className="App-link"
           href="https://plataforma.coderhouse.com"
@@ -22,10 +29,15 @@ function App() {
           CoderHouse
         </a>
       </header>
-      
+      <section style={{ marginBottom: 10 }}>
+        <Button onClick={ () => setItemsQty (itemsQty - 1 ) } variant="success">-</Button>
+        <span style={{ margin: 10, fontSize: "3rem" }}>{itemsQty}</span>
+        <Button onClick={ () => setItemsQty (itemsQty + 1 ) } variant="success">+</Button>
+      </section>
     </div>
     
   );
 }
+
 
 export default App;
